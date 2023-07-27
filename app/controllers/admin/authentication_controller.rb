f23aa418f86ad040a2cb5dd66fda73d1f0674d48
Paulo Campos Filho <pezaum@gmail.com>
2023-07-26 23:22:02 -0700

    Fix :name keyword on authentication_controller. Make admin controllers inherit from this new controller.

--- app/controllers/admin/authentication_controller.rb
+++ app/controllers/admin/authentication_controller.rb
@@ -1,3 +1,3 @@
 class Admin::AuthenticationController < ApplicationController
-  http_basic_authenticate_with username: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 end
