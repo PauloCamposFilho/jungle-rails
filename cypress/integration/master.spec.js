9e0b579291acd2fcc58f95ef1c2ab4e6bf851fd7
Paulo Campos Filho <pezaum@gmail.com>
2023-08-03 05:57:37 -0700

    Consolidate tests into master.spec.js

---
+++ cypress/integration/master.spec.js
@@ -0,0 +1,6 @@
+import './home.spec'
+import './product_details.spec'
+import './add_to_cart.spec'
+import './user_login.spec'
+
+// Master test file. import all individual tests here. Run this file for the full tests.
